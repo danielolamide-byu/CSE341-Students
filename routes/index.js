85f7d2c598ef40b9e7d975979b4038a8df9ef371
danielolamide-byu <olamidedaniel874@gmail.com>
2025-11-30 22:53:17 +0100

    Route.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const passport = require("passport");
+const passport = require('passport');
 
 const router = require("express").Router();
 
--- routes/personalnfo.js
+++ routes/personalnfo.js
@@ -11,7 +11,7 @@ const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', personalInfo.getAll);
 router.get('/:id', personalInfo.getSingle);
-router.post('/', isAuthenticated, personalInfo.createPersonalInfo);
+router.post('/', personalInfo.createPersonalInfo);
 router.put('/:id', personalInfo.updatePersonalInfo);
 router.delete('/:id', personalInfo.deletePersonalInfo);
 
