7e57e7dad87220287f1cb89008b4da947625871c
danielolamide-byu <olamidedaniel874@gmail.com>
2025-11-30 22:10:07 +0100

    Session

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,16 @@ const router = require("express").Router();
 
 router.use('/', require('./swagger'));
 
-// router.get('/', (req, res) => {
-//     res.send('Hello World Right Now!!');
-// });
+router.get('/', (req, res) => {
+    res.send('Hello World Right Now!!');
+});
 
 router.use('/course', require('./courses'));
 router.use('/info', require('./personalnfo'))
 
 router.get('/login',
   passport.authenticate('github'), (req, res) => {
-    // // This callback only runs if authentication is successful
-    // res.send("Logged innnnn"); // Redirect to a protected route after successful login
+   
   }
 );
 
